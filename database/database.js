191b0d572eef9c6a4ecf46c3b60cf6534058bd28
Laweeza <louieshing@gmail.com>
2021-10-18 17:53:17 -0700

    established connection with test database

--- database/database.js
+++ database/database.js
@@ -11,3 +11,12 @@ const pw = config.pw
 const db = config.db
 const port = config.port
 const conString = `postgres://${user}:${pw}@${host}:${port}/${db}`
+
+//Connect to DB
+const client = new Client({
+  connectionString: conString,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!')
+});
