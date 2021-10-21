6bb93b05fd21d54d3bab377395fb05f88ae989cf
Laweeza <louieshing@gmail.com>
2021-10-20 20:10:56 -0700

    Imported csv files to tables

--- database/db.js
+++ database/db.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
-const path = require('path')
 const {Pool} = require('pg')
+const copyFrom = require('pg-copy-streams').from
 const config = require('./../config.json')
 
+//inputfile
+// const table = 'questions'
 
 //Get connection parameters
 const host = config.host
@@ -13,13 +15,45 @@ const port = config.port
 const conString = `postgres://${user}:${password}@${host}:${port}/${database}`
 
 //Connect to DB
-const db = new Pool({
+const pool = new Pool({
   connectionString: conString,
 });
 
-db.connect(function(err) {
-  if (err) throw err;
-  console.log(`Database connected to ${port}!`)
-});
+// const query =`
+// SELECT * FROM answers WHERE answers.question_id = 1;
+// `
+//   (async () => {
+//     try {
+//       const client = await pool.connect();
+//       const res = await client.query(query);
+//       for (let row of res.rows) {
+//         console.log(row);
+//       }
+//     }
+//     catch(err) {
+//       console.error(err);
+//     }
+//   })();
 
-module.exports = db;
+
+
+  // let stream = pool.query(copyFrom(`COPY questions FROM DELIMITER ',' CSV HEADER STDIN`))
+  // let fileStream = fs.createReadStream('/Users/louisa/questions.csv')
+
+  // fileStream.on('error', (error) => {
+  //   console.log(`Error in reading file: ${error}`)
+  // })
+  // stream.on('error', (error) => {
+  //   console.log(`Error in copy command: ${error}`)
+  // })
+  // stream.on('end', () => {
+  //   console.log(`Completed loading data into table`)
+  // })
+  // fileStream.pipe(stream)
+
+
+
+
+
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const db = require('../database/db.js')
+const pool = require('../database/db.js')
 const cors = require('cors')
 
 const app = express()
@@ -14,6 +14,24 @@ app.get('/', (req, res) => {
   res.send('Hello')
 })
 
+//ROUTES//
+
+//GET /qa/questions
+
+//GET /qa/questions/:question_id/answers
+
+
+//POST /qa/questions
+
+//POST /qa/questions/:question_id/answers
+
+//PUT /qa/questions/:question_id/helpful
+
+//PUT /qa/questions/:question_id/report
+
+//PUT /qa/answers/:answer_id/helpful
+
+//PUT /qa/answers/:answer_id/report
 
 
 
