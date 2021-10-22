b2cf224a92345781f12ca693327bb36754c68fd7
Laweeza <louieshing@gmail.com>
2021-10-22 15:53:58 -0700

    uninstalled unsued dependencies

--- routes.js
+++ routes.js
@@ -13,22 +13,22 @@ router.get('/qa/questions', helper.getQuestions);
 router.get('/qa/questions:question_id/answers', helper.getAnswers);
 
 
-router.post('/qa/questions', helper.addQuestion)
+router.post('/qa/questions', helper.addQuestion);
 
 
-router.post('/qa/questions/:question_id/answers', helper.addAnswer)
+router.post('/qa/questions/:question_id/answers', helper.addAnswer);
 
 
-router.put('/qa/questions/:question_id/helpful', helper.markQuestion)
+router.put('/qa/questions/:question_id/helpful', helper.markQuestion);
 
 
-router.put('/qa/questions/:question_id/report', helper.reportQuestion)
+router.put('/qa/questions/:question_id/report', helper.reportQuestion);
 
 
-router.put('/qa/answers/:answer_id/helpful', helper.markAnswer)
+router.put('/qa/answers/:answer_id/helpful', helper.markAnswer);
 
 
-router.put('/qa/answers/:answer_id/report', helper.reportAnswer)
+router.put('/qa/answers/:answer_id/report', helper.reportAnswer);
 
 
 module.exports = router;
