79fca4fed8d3fed269b92452dcc9599b6618228e
Laweeza <louieshing@gmail.com>
2021-10-22 22:00:26 -0700

    created structure for answers

--- controllers/answers.js
+++ controllers/answers.js
@@ -1,1 +1,19 @@
 const models = require('../models')
+
+module.exports ={
+  get: (req, res) => {
+
+  },
+
+  post: (req, res) => {
+
+  },
+
+  helpful: (req, res) => {
+
+  },
+
+  reported: (req, res) => {
+
+  }
+}
--- models/answers.js
+++ models/answers.js
@@ -1,4 +1,4 @@
-const {query} = require('./../database/db.js')
+const {pool} = require('./../database/db.js')
 
 module.exports = {
 
