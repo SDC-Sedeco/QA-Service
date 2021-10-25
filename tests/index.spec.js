976f7b4cf65b344c093be9bd05302df520c3b652
Laweeza <louieshing@gmail.com>
2021-10-25 01:10:34 -0700

    set up mock for unit tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,39 +1,36 @@
 const { Pool } = require('pg')
-const { client } = require('./../database/db.js')
 
-describe('route', function () {
-  let app; //eslint-disable-line
-  before('mock connection', async function () { //eslint-disable-line
-    const pool = new Pool({
-      host: 'testpostgres',
-      user: 'testuser',
-      pw: '',
-      db: 'test',
-      port: 5432,
-      pool: {
-        max: 1,
-        idleTimeoutMillis: 0
-      }
-    })
-  client.query = (text, values) => {
-    return pool.query(text, values)
+//setup mock for unit tests
+jest.mock('pg', () =>{
+  const mockPool = {
+    connect: function () {
+      return {query: jest.fn()}
+    },
+    query: jest.fn(),
+    end: jest.fn(),
+    on: jest.fn()
   }
-
-  app = require('./../server/index.js')
+  return { Pool: jest.fn(() => mockPool)}
 })
 
- beforeEach('Create temp table', async function () {
-   await client.query('CREATE TEMPORARY TABLE questions (LIKE questions INCLUDING ALL)')
- })
+describe('test', () => {
+  let pool;
 
- afterEach('Drop temporary tables', async function () {
-   await client.query('DROP TABLE IF EXISTS pg_temp.questions')
- })
+  beforeEach(() => {
+    pool = new Pool();
+  });
 
-describe('POST /qa/questions', function () {
-  it('Should create a question')
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it ('test', async () => {
+    pool.query.mockResolvedValueOnce({rows: [], rowCount: 0});
+  })
 })
 
 
 
-})
+
+
+
