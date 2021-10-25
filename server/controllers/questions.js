a3012d2531e8661d64e094926bcec2b268452346
Laweeza <louieshing@gmail.com>
2021-10-25 16:06:47 -0700

    finally transformed GET API data for questions to correct structure in server

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -3,8 +3,23 @@ const models = require('../models')
 module.exports = {
   get: (req, res) => {
     console.log(req.query.product_id, req.query.page, req.query.count)
+
     models.questions.get(req.query)
     .then(({rows}) => {
+
+      for (let i = 0; i < rows.length; i++) {
+
+        let answerArr = rows[i].answers
+
+        let answerObject = answerArr.reduce((answerObj, data) => {
+         answerObj[data.id] = data
+        //  console.log('answers', answerObj)
+        return answerObj}, {} )
+
+        rows[i].answers = answerObject
+
+      }
+
       rows.length === 0 ? res.sendStatus(500)
       : res.status(200).send(
         {
