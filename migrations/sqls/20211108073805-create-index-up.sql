ec603359385c66cbffaebb7b1efafb562fd4321f
Laweeza <louieshing@gmail.com>
2021-11-08 19:00:29 -0800

    reduced more migration files

--- migrations/sqls/20211108073805-create-index-up.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_product_id ON questions(product_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_question_id ON answers(question_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_answer_id ON photos(answer_id);
-
-
--- seeder/seed_csv.sql
+++ seeder/seed_csv.sql
@@ -30,6 +30,7 @@ ALTER TABLE questions SET UNLOGGED;
 COPY questions FROM '/Users/louisa/HR/HR2021/SDC/QA-Service/csv/questions.csv' DELIMITER ',' CSV HEADER;
 ALTER TABLE questions ALTER COLUMN date TYPE timestamp with time zone USING (to_timestamp(date/1000) AT TIME ZONE 'UTC');
 ALTER TABLE questions ALTER COLUMN date SET DEFAULT CURRENT_TIMESTAMP;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_product_id ON questions(product_id);
 ALTER TABLE questions SET LOGGED;
 
 -- COPY CSV DATA FOR answers AND SET CONSTRAINTS
@@ -38,10 +39,22 @@ COPY answers FROM '/Users/louisa/HR/HR2021/SDC/QA-Service/csv/answers.csv' DELIM
 ALTER TABLE answers ALTER COLUMN date TYPE timestamp with time zone USING (to_timestamp(date/1000) AT TIME ZONE 'UTC');
 ALTER TABLE answers ALTER COLUMN date SET DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE answers ADD CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES questions(id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_question_id ON answers(question_id);
 ALTER TABLE answers SET LOGGED;
 
 -- COPY CSV DATA FOR photos AND SET CONSTRAINTS
 ALTER TABLE photos SET UNLOGGED;
 COPY photos FROM '/Users/louisa/HR/HR2021/SDC/QA-Service/csv/answers_photos.csv' DELIMITER ',' CSV HEADER;
 ALTER TABLE photos ADD CONSTRAINT fk_answer FOREIGN KEY (answer_id) REFERENCES answers(id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_answer_id ON photos(answer_id);
 ALTER TABLE photos SET LOGGED;
+
+
+-- CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_product_id ON questions(product_id);
+-- CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_question_id ON answers(question_id);
+-- CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_answer_id ON photos(answer_id);
+
+-- DROP INDEX idx_product_id;
+-- DROP INDEX idx_question_id;
+-- DROP INDEX idx_answer_id;
+
