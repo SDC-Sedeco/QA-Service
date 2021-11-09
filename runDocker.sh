9de499963192575762b88794da7a71cc161d8436
Laweeza <louieshing@gmail.com>
2021-11-08 19:13:44 -0800

    more cleanup

--- runDocker.sh
+++ runDocker.sh
@@ -1,12 +1,3 @@
 docker-compose down
 docker-compose up --build -d
 # sleep 5 # seconds
-
-
-
-# docker logs sdc-qa-server-1 -f
-# docker exec -it sdc-qa-server-1 /bin/sh
-# ps aux| grep node
-# docker exec -it sdc-db-1 /bin/sh
-# docker logs sdc-db-1 -f
-# docker cp ./csv sdc-db-1:/csv
