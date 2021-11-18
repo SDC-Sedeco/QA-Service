8ebc990ee07ca4b6655ef06d06b0c01c02477a67
Laweeza <louieshing@gmail.com>
2021-11-18 13:06:17 -0800

    created sample nginx k6 test to test proxy

---
+++ __tests__/k6scripts/nginx.js
@@ -0,0 +1,12 @@
+import { check } from "k6";
+import http from 'k6/http';
+
+export default function() {
+  var url = "http://localhost:80/";
+  let res = http.get(url);
+  check(res, {
+    "is status 200": (r) => r.status === 200
+  },{ my_tag: res.body },);
+}
+
+// k6 run -u 200 -d 30s __tests__/k6scripts/nginx.js
