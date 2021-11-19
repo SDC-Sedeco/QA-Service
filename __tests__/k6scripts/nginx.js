65431cf505833d7658776b26712990ab5e08824a
Laweeza <louieshing@gmail.com>
2021-11-19 12:31:16 -0800

    testing lb

--- __tests__/k6scripts/nginx.js
+++ __tests__/k6scripts/nginx.js
@@ -2,7 +2,7 @@ import { check } from "k6";
 import http from 'k6/http';
 
 export default function() {
-  var url = "http://localhost:80/";
+  var url = "http://localhost:80/api/qa/questions/3518957/answers";
   let res = http.get(url);
   check(res, {
     "is status 200": (r) => r.status === 200
